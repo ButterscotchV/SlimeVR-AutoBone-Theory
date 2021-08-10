93ae3f1cf594f06bfddaf228ac2b81bea8ff2e52
ButterscotchVanilla <bscotchvanilla@gmail.com>
2021-08-10 04:00:36 -0400

    Initial project commit

---
+++ slimevr-auto-bone/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace slimevr_auto_bone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
